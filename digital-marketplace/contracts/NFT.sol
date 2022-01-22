8cff162d9dd7fc2caaef27375b249ad8403abfb2
tawseefnabi <tawseefnabi9@gmail.com>
2022-01-22 16:33:39 +0530

    defining NFT contract

--- digital-marketplace/contracts/NFT.sol
+++ digital-marketplace/contracts/NFT.sol
@@ -2,11 +2,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-import "hardhat/console.sol";
 
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
