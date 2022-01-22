1c07a726ddd64120025a7e1de0bca58ff0c54641
tawseefnabi <tawseefnabi9@gmail.com>
2022-01-22 16:43:30 +0530

    defining NFT contract

--- digital-marketplace/contracts/NFT.sol
+++ digital-marketplace/contracts/NFT.sol
@@ -8,18 +8,20 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract NFT is ERC721URIStorage {
+
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter public _tokenIds;
     address contractAddress;
 
-    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT")  {
         contractAddress = marketplaceAddress;
     }
-
-    function createToken(string memory tokenURI) public returns (uint) {
+    // create a new token
+    function createToken(string memory tokenURI) public  returns (uint){
+     // increment tokenids
         _tokenIds.increment();
+        // mint token
         uint256 newItemId = _tokenIds.current();
-
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
         setApprovalForAll(contractAddress, true);
